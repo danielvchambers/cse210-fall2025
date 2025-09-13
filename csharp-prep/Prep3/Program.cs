e5eeb8a2ebbe11178d7ce9845c67c88243c5ef9f
Daniel Chambers <cha24074@byui.edu>
2025-09-13 13:10:01 -0800

    Prep1 and 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random magicGenerator = new Random();
+        int magicNumber = magicGenerator.Next(1, 101);
+        bool quit = false;
+        int numberGuesses = 0;
+        bool restart = false;
+        while (!restart)
+        {
+            while (!quit)
+            {
+                numberGuesses = numberGuesses + 1;
+                Console.Write("What is Your guess? ");
+                string numberGuess = Console.ReadLine();
+                int guess = int.Parse(numberGuess);
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Greater");
+                }
+                else if (guess == magicNumber)
+                {
+                    Console.WriteLine("You Got it!!!");
+                    Console.WriteLine($"The number of times you guessed: {numberGuesses}");
+                    quit = true;
+                }
+            }
+            Console.Write("Would you like to play again? (y, n) ");
+            string cont = Console.ReadLine();
+            quit = false;
+            if (cont != "y")
+            {
+                Console.WriteLine("Bye");
+                restart = true;
+            }
+        }
     }
 }
