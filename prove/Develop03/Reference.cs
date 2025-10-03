c4550135d4e5c4e686cc91cb2e291a7a70e11ac7
Daniel Chambers <cha24074@byui.edu>
2025-10-03 15:52:07 -0800

    Reference class functional

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -6,16 +6,67 @@ public class Reference
 
     public Reference(string Reference)
     {
-
+        ParseReference(_sReference);
     }
 
     private void ParseReference(string _sReference)
     {
-
+        _sReference = _sReference.Replace(" ", "");
+        bool _isBook = true;
+        bool _isChapter = false;
+        bool _isVerse = false;
+        for (int index = 0; index < _sReference.Count(); index++)
+        {
+            if (_isBook == true)
+            {
+                if (index == 0 && char.IsDigit(_sReference[index]))
+                {
+                    _book += _sReference[index];
+                }
+                else if (char.IsDigit(_sReference[index]))
+                {
+                    _isBook = false;
+                    _isChapter = true;
+                    _chapter += _sReference[index];
+                }
+                else
+                {
+                    _book += _sReference[index];
+                }
+            }
+            else if (_isChapter == true)
+            {
+                if (char.IsDigit(_sReference[index]))
+                {
+                    _chapter += _sReference[index];
+                }
+                else if (char.ToString(_sReference[index]) == ":")
+                {
+                    _isChapter = false;
+                    _isVerse = true;
+                }
+            }
+            else if (_isVerse == true)
+            {
+                if (char.ToString(_sReference[index]) == ",")
+                {
+                    _verse += $"{_sReference[index]} ";
+                }
+                else if (char.ToString(_sReference[index]) == "-")
+                {
+                    _verse += _sReference[index];
+                }
+                else
+                {
+                    _verse += _sReference[index];
+                }
+            }
+        }
     }
 
     public string GetReference()
     {
-
+        string strReference = $"{_book} {_chapter}:{_verse} -> ";
+        return strReference;
     }
 }
