8597b383fb5ffdc9549b7f87149f4772d14cc081
Daniel Chambers <cha24074@byui.edu>
2025-10-03 09:30:09 -0800

    base design for the classes scripture and Reference

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,4 +1,21 @@
 public class Reference
 {
+    private string _book;
+    private string _chapter;
+    private string _verse;
 
+    public Reference(string Reference)
+    {
+
+    }
+
+    private void ParseReference(string _sReference)
+    {
+
+    }
+
+    public string GetReference()
+    {
+
+    }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,4 +1,36 @@
 public class Scripture
 {
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+    private Random _random = new Random();
 
+    public Scripture(string reference, string scripture)
+    {
+
+    }
+
+    public void DisplayScripture()
+    {
+
+    }
+
+    public int PEnd()
+    {
+
+    }
+
+    public void HideRandomWords(int numberOFWordsToHide = 3)
+    {
+
+    }
+
+    public int AddInt()
+    {
+
+    }
+
+    public bool AllWordsHidden()
+    {
+
+    }
 }
