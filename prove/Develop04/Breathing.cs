99a4245655b30f2d0c2f120cb9ffaa51b1c5527c
Daniel Chambers <cha24074@byui.edu>
2025-10-08 20:07:36 -0800

    Breathing Activity

--- prove/Develop04/Breathing.cs
+++ prove/Develop04/Breathing.cs
@@ -2,11 +2,19 @@ public class Breathing : Activities
 {
     public Breathing(int time) : base(time)
     {
-
+        base._activityName = "Breathing Exercise";
+        base._activityInstr = "Clear Your mind and take deep breathes according to the Timeing in the activity.";
     }
 
     public void Run()
     {
-
+        DateTime _startTime = DateTime.Now;
+        DateTime _endTime = _startTime.AddSeconds(_activityRunTime);
+        while (DateTime.Now <= _endTime)
+        {
+            Console.WriteLine("Breathe in");
+            base.WaitingAnim();
+            Console.WriteLine("Breathe out");
+        }
     }
 }
